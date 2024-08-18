e3560ee1289137a428504844f7265a85ccdb6077
Christoph Tempelhoff <Christoph.Tempelhoff@gmail.com>
2024-08-18 18:06:42 +0200

    Projektdateien hinzufügen.

---
+++ AbwesendheitsprogrammReworked/Controllers/LogInController.cs
@@ -0,0 +1,28 @@
+﻿using AbwesendheitsprogrammReworked.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbwesendheitsprogrammReworked.Controllers
+{
+    internal class LogInController
+    {
+        User model;
+        MainWindow view;
+
+        public LogInController(User model, MainWindow view)
+        {
+            this.model = model;
+            this.view = view;
+
+            this.instiateTables();
+        }
+
+        private void instiateTables() 
+        {
+            model.executeQuerry("CreateTables");
+        }
+    }
+}
---
+++ AbwesendheitsprogrammReworked/Models/User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MySql.Data.MySqlClient;
+
+namespace AbwesendheitsprogrammReworked.Models
+{
+    internal class User
+    {
+        private int ID;
+        private string Name;
+        private string Email;
+        private string Password;
+        private string PhoneNumber;
+        private Absenty? Absenty;
+
+        public User(int id, string name,  string email, string password, string phoneNumber)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.Email = email;
+            this.Password = password;
+            this.PhoneNumber = phoneNumber;
+            this.Absenty = null;
+        } 
+
+        public void executeQuerry(string querryToExecute, bool isProcedure = false)
+        {
+            using (MySqlConnection conn = new MySqlConnection(Properties.Settings.Default.ConnectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand(querryToExecute, conn);
+                    if (isProcedure)
+                    {
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    }
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                    throw;
+                }
+            }
+        }
+    }
+}
